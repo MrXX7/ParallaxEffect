836c37356ca8764ac254f0ebf44414bfb7c20d9f
Oncu <oohancan@gmail.com>
2024-07-18 22:34:21 +0300

    Initial Commit

---
+++ ParallaxEffect/ParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectApp.swift
+//  ParallaxEffect
+//
+//  Created by Oncu Can on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
