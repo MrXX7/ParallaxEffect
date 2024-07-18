9b164b47158b40e20e910b98ce36679e13007838
Oncu <oohancan@gmail.com>
2024-07-18 23:15:52 +0300

    Add ContentView for displaying a horizontal scrollable movie poster list
    - Create ContentView with a horizontal scrollable list of movie posters
    - Initialize an array of image names and titles for the posters
    - Use GeometryReader to dynamically calculate dimensions and offsets
    - Add a title "Movie Universe" at the top
    - Implement a horizontal ScrollView containing movie posters with interactive scroll transition effects
    - Apply LinearGradient and text overlays on the posters for visual enhancement
    - Add shadows to the posters based on the color scheme
    - Implement a preview with sample data

--- ParallaxEffect/ContentView.swift
+++ ParallaxEffect/ContentView.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let images = (1...7).map {"\($0)"}
+    private let titles = [
+    "Oppenheimer",
+    "Saw X",
+    "Poor Things",
+    "The Zone of Interest",
+    "Indiana Jones",
+    "Spider Man",
+    "The Creator"
+    ]
+    @Environment(\.colorScheme) private var colorScheme
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        GeometryReader(content: { geometry in
+            let width = geometry.size.width
+            let height = 1.25 * width
+            let offset = -width/2
+            
+            VStack(spacing: 20) {
+                Text("Movie Universe")
+                    .font(.title)
+                    .bold()
+                
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 22, content: {
+                        ForEach(images.indices, id: \.self) { index in
+                            ZStack {
+                                MoviePosterView(imageName: images[index])
+                                    .frame(width: width, height: height)
+                                    .scrollTransition(.interactive, axis: .horizontal) { content, phase in
+                                        content.offset(x: phase.value * offset)
+                                    }
+                                    .overlay {
+                                        LinearGradient(colors: [.clear, .black], startPoint: .top, endPoint: .bottom)
+                                    }
+                                    .overlay(alignment: .bottom) {
+                                        Text(titles[index])
+                                            .font(.title)
+                                            .bold()
+                                            .foregroundStyle(.white)
+                                            .padding(.bottom, 44)
+                                    }
+                            }
+                            .containerRelativeFrame(.horizontal)
+                            .clipShape(RoundedRectangle(cornerRadius: 32))
+                            .shadow(color: colorScheme == .dark ? .white.opacity(0.5) : .black.opacity(0.5), radius: 5, x: 0, y: 2)
+                        }
+                    })
+                    .scrollTargetLayout()
+                    .padding([.top, .bottom], 20)
+                }
+                .frame(height: height)
+                .contentMargins(.horizontal, 44)
+                .scrollTargetBehavior(.paging)
+            }
+        })
     }
 }
 
